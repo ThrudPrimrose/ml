0302305affbf7b726f570d302a2721074c7b5928
Yakup Budanaz <yakup.paradox@gmail.com>
2021-04-19 14:01:22 +0200

    update interpolation to higher degrees

--- gaussian.py
+++ gaussian.py
@@ -11,11 +11,12 @@ def least_squares(xs, ys, a) -> float:
 
     return _sum
 
+
 def plot_linear():
     # x is uniform distributed
     xs = np.random.uniform(0, 1, 1000)
     # error normal distributed
-    es = np.random.normal(0.5, 0.25, 1000)
+    es = np.random.normal(0, 0.25, 1000)
 
     # y locations
     distributed = np.add(xs, es)
@@ -26,13 +27,26 @@ def plot_linear():
     m, b = np.polyfit(xs, distributed, 1)
     plt.plot(xs, m * xs + b, color='green', linewidth=5, markersize=15)
 
+    xm = np.mean(xs)
+    ym = np.mean(distributed)
+    slope = ym / xm
+
+    plt.plot(xs, slope*xs, color='red', linewidth=5, markersize=15)
+
+    xsum = np.mean(xs)
+    ysum = np.mean(distributed)
+    slope_sums = ysum / xsum
+
+    plt.plot(xs, slope_sums*xs, color='purple', linewidth=5, markersize=15)
+
     # opens gui and shows
     # plt.show()
     # I want it in a pdf file though
     plot.figure.savefig('scatterplot-linear.pdf')
 
-    #clear the figure
+    # clear the figure
     plt.clf()
+
 
 def plot_quadratic():
     # x is uniform distributed
@@ -45,12 +59,44 @@ def plot_quadratic():
     distributed = 30*(((xs-0.25)*(xs-0.25)) * ((xs - 0.75)*(xs - 0.75))) + es
     quad = plt.scatter(xs, distributed)
 
+    # poly degree 3
+    xs2 = np.square(xs)
+    xs3 = np.power(xs, 3)
+    xs4 = np.square(xs2)
+    x_line = np.linspace(0, 1, 100)
+
+    X1 = np.c_[np.ones(1000), xs]
+    # * -> element wise mul, @ -> matrix mul
+    A1 = np.linalg.inv(X1.T @ X1) @ X1.T @ distributed
+    y_pred = A1[0] + A1[1]*x_line
+    plt.plot(x_line, y_pred, color="green")
+
+    X2 = np.c_[np.ones(1000), xs, xs2]
+    A2 = np.linalg.inv(X2.T @ X2) @ X2.T @ distributed
+    y_pred = A2[0] + A2[1]*x_line + A2[2]*np.square(x_line)
+    plt.plot(x_line, y_pred, color="blue")
+
+    X3 = np.c_[np.ones(1000), xs, xs2, xs3]
+    A3 = np.linalg.inv(X3.T @ X3) @ X3.T @ distributed
+    y_pred = A3[0] + A3[1]*x_line + A3[2] * \
+        np.square(x_line) + A3[3]*np.power(x_line, 3)
+    plt.plot(x_line, y_pred, color="red")
+
+    X4 = np.c_[np.ones(1000), xs, xs2, xs3, xs4]
+    A4 = np.linalg.inv(X4.T @ X4) @ X4.T @ distributed
+    y_pred = A4[0] + A4[1]*x_line + A4[2] * \
+        np.square(x_line) + A4[3]*np.power(x_line, 3) + \
+        A4[4]*np.power(x_line, 4)
+    plt.plot(x_line, y_pred, color="purple")
+
     quad.figure.savefig('scatterplot-quadratic.pdf')
     plt.clf()
+
 
 def main():
     plot_linear()
     plot_quadratic()
+
 
 if __name__ == "__main__":
     # execute only if run as a script
